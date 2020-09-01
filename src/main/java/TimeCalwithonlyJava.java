be0acb0845dc27abdc93140e7346478561f4534a
li8max <sivadatta1204@gmail.com>
2020-09-01 18:21:17 +0530

    Initial commit

---
+++ src/main/java/TimeCalwithonlyJava.java
@@ -0,0 +1,14 @@
+public class TimeCalwithonlyJava {
+
+    public static void main(String[] args){
+        long avgonlyjava=0;
+        for (int i=0;i<1000;i++){
+            long starttime=System.nanoTime();
+            System.out.println("Hello World!! Using only JAVA!!");
+            long estimatedtime=System.nanoTime() -starttime;
+            //System.out.println("Time taken for simple java program is:"+estimatedtime);
+            avgonlyjava+=estimatedtime/1000;
+        }
+        System.out.println(avgonlyjava);
+    }
+}
