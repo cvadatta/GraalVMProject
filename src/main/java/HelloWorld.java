9a871c8c7e87aa65376478849d03a8ff9a4b8309
li8max <sivadatta1204@gmail.com>
2020-09-02 13:35:39 +0530

    Added Readme file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,6 @@
-
-import org.graalvm.polyglot.*;
-
-
 public class HelloWorld {
-
-    context polyglot=context.create();
-    polyglot.eval("js","print ('Hello JS from Graalvm!');");
+    public static void main(String[] args){
+        Context polyglot = Context.create();
+        polyglot.eval("js", "print('Hello JavaScript!')");
+    }
 }
