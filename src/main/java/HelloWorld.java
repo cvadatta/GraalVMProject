e5a879485755f33c2f74db992c9b0c8f2389c9c7
li8max <sivadatta1204@gmail.com>
2020-09-03 13:45:42 +0530

    Returned json from JS to JAVA

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,11 @@ import org.graalvm.polyglot.Context;
 
 public class HelloWorld {
     public static void main(String[] args){
-        Context polyglot = Context.create();
-        polyglot.eval("js", "print('Hello JavaScript!')");
+
+        Context c = Context.create();
+        c.eval("js", "print('Hello JavaScript!')");
     }
+
+
 
 }
---
+++ src/main/java/ReturnTask.java
@@ -0,0 +1,64 @@
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ReturnTask {
+    public static void main(String[] args){
+        Context c= Context.create();
+        try {
+            File Jsonjs=new File("src\\main\\resources\\Json.js");
+            c.eval(Source.newBuilder("js",Jsonjs).build());
+            ObjectNode data = createJsonNode();
+            Value res=c.getBindings("js").getMember("returnsJson");
+            Value resf=res.execute(data);
+            System.out.println(resf);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+/*    public static void main(String[] args)
+            throws Exception
+    {
+        ScriptEngine ee = new ScriptEngineManager()
+                .getEngineByName("nashorn");
+        ee.eval(new FileReader("src\\main\\resources\\Json.js"));
+        Invocable invocable=(Invocable)ee;
+
+        ObjectNode data = createJsonNode();
+        //invocable.invokeFunction("receiveJson",data);
+        Object res=invocable.invokeFunction("returnsJson",data);
+        System.out.println(res);
+        System.out.println(res.getClass());
+        System.out.println(res.toString());
+        ObjectMapper mapper=new ObjectMapper();
+        String fin=mapper.writeValueAsString(res);
+        System.out.println(fin);
+
+        JsonNode jsonNode = mapper.readTree(fin);
+        System.out.println(jsonNode.toString());
+
+
+    }
+
+ */
+
+    public static ObjectNode createJsonNode() {
+        ObjectMapper objectMapper = new ObjectMapper(new JsonFactory());
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        objectNode.put("firstName", "Siva");
+        objectNode.put("lastName", "Datta");
+
+        return objectNode;
+    }
+
+}
