6e2acc4cabb3f599a2f9c59ddb7a5f8887a64326
li8max <sivadatta1204@gmail.com>
2020-10-08 10:25:32 +0530

    ..

--- src/main/resources/fetch.js
+++ src/main/resources/fetch.js
@@ -1,3 +1,41 @@
+var tch=function(){
+    var uril = 'https://egov-micro-dev.egovernments.org/egov-mdms-service/v1/_search';
+
+    // post body data
+    var user = {
+      "RequestInfo": {},
+      "MdmsCriteria": {
+        "tenantId": "pb",
+        "moduleDetails": [
+          {
+            "moduleName": "tenant",
+            "masterDetails": [
+              {
+                "name": "tenants",
+                "filter": "$.*.code"
+              }
+            ]
+          }
+        ]
+      }
+    };
+    // request options
+    var options = {
+        method: 'POST',
+        //mode: 'no-cors',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // send POST request
+    fetch(uril, options)
+    .then(res => res.json())
+    .then(res => console.log(res));
+
+};
+
 /*
 var tch=function(){
 
@@ -90,41 +128,4 @@ var tch=function(){
 //}
 
 
-var tch=function(){
-    var uril = 'https://egov-micro-dev.egovernments.org/egov-mdms-service/v1/_search';
-
-    // post body data
-    var user = {
-      "RequestInfo": {},
-      "MdmsCriteria": {
-        "tenantId": "pb",
-        "moduleDetails": [
-          {
-            "moduleName": "tenant",
-            "masterDetails": [
-              {
-                "name": "tenants",
-                "filter": "$.*.code"
-              }
-            ]
-          }
-        ]
-      }
-    };
-    // request options
-    var options = {
-        method: 'POST',
-        //mode: 'no-cors',
-        body: JSON.stringify(user),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-
-    // send POST request
-    fetch(uril, options)
-    .then(res => res.json())
-    .then(res => console.log(res));
-
-};
 
